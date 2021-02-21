049e1a53c3e552c7d6b01eaf1fb78cca251d6977
Raheem <raheemideadev@gmail.com>
2021-02-21 17:27:04 +0500

    injection

---
+++ app/src/main/java/xyz/teamgravity/spotify/injection/App.kt
@@ -0,0 +1,7 @@
+package xyz.teamgravity.spotify.injection
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App: Application()
---
+++ app/src/main/java/xyz/teamgravity/spotify/injection/ApplicationModule.kt
@@ -0,0 +1,28 @@
+package xyz.teamgravity.spotify.injection
+
+import android.content.Context
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
+import com.bumptech.glide.request.RequestOptions
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import xyz.teamgravity.spotify.R
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object ApplicationModule {
+
+    @Singleton
+    @Provides
+    fun provideGlide(@ApplicationContext context: Context) = Glide.with(context).setDefaultRequestOptions(
+        RequestOptions()
+            .placeholder(R.drawable.placeholder_image)
+            .error(R.drawable.placeholder_error)
+            .diskCacheStrategy(DiskCacheStrategy.DATA)
+    )
+
+}
