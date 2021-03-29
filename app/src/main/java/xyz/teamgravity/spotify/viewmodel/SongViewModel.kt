d5c8131eb5cb169f25a74a2f8a6f1438ffaaf4d0
Raheem <raheemideadev@gmail.com>
2021-03-29 16:15:56 +0500

    song view model
    Took 45 minutes

---
+++ app/src/main/java/xyz/teamgravity/spotify/viewmodel/SongViewModel.kt
@@ -0,0 +1,45 @@
+package xyz.teamgravity.spotify.viewmodel
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import xyz.teamgravity.spotify.helper.util.MusicServiceConnection
+import xyz.teamgravity.spotify.helper.util.currentPlaybackPosition
+import xyz.teamgravity.spotify.service.MusicService
+import javax.inject.Inject
+
+@HiltViewModel
+class SongViewModel @Inject constructor(
+    private val musicServiceConnection: MusicServiceConnection
+) : ViewModel() {
+
+    private val playbackState = musicServiceConnection.playbackState
+
+    private val _curSongDuration = MutableLiveData<Long>()
+    val curSongDuration: LiveData<Long> = _curSongDuration
+
+    private val _curPlayerPosition = MutableLiveData<Long>()
+    val curPlayerPosition: LiveData<Long> = _curPlayerPosition
+
+    init {
+        updateCurrentPlayerPosition()
+    }
+
+    private fun updateCurrentPlayerPosition() {
+        viewModelScope.launch {
+            while (true) {
+                val position = playbackState.value?.currentPlaybackPosition
+                if (curPlayerPosition.value != position) {
+                    position?.let { _curPlayerPosition.postValue(it) }
+                    _curSongDuration.postValue(MusicService.currentSongDuration)
+                }
+
+                delay(100L)
+            }
+        }
+    }
+}
